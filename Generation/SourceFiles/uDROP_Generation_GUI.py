98a7fd23f598376e5b6b24a04024985a247b7199
chrizrodz <chrizrodz@gmail.com>
2017-07-31 13:55:24 -0500

    Changed the GUI to be object orientated.

--- Generation/SourceFiles/uDROP_Generation_GUI.py
+++ Generation/SourceFiles/uDROP_Generation_GUI.py
@@ -14,231 +14,239 @@ import tkinter
 from tkinter import ttk
 import cv2
 import subprocess
-
-
-
-if(os.path.exists("frames/")):
-	shutil.rmtree('frames/')
-os.mkdir('frames/')
-os.system("ffmpeg -i " + sys.argv[1] + " -vf mpdecimate,setpts=N/FRAME_RATE/TB frames/%03d.png")
-
-buf = []
-for f in sorted(os.listdir("frames")):
-	buf.append(cv2.imread("frames/"+f))
+import numpy
+from tqdm import tqdm
 
 
 
 
-pic_width = 0
-pic_height = 0
-bound_top_left = [-1,-1]
-bound_bottom_right = [-1,-1]
-channel_selections = [[0,0],[0,0]]
-select_index = 0
+class SetupGUI:
 
-def click(event):
-	global bound_top_left
-	global bound_bottom_right
-	global select_index
-	global resize_ratio
-	modx = event.x * resize_ratio
-	mody = event.y * resize_ratio
-	if select_index == 0:
-		bound_top_left = [modx,mody]
-		select_index += 1
-	elif select_index == 1:
-		bound_bottom_right = [modx,mody]
-		drawGUI()
-		select_index += 1
-	elif select_index == 2:
-		channel_selections[0][0] = modx
-		channel_selections[0][1] = mody
-		select_index += 1
-	elif select_index == 3:
-		channel_selections[1][0] = modx
-		channel_selections[1][1] = mody
-		select_index += 1
-	drawGUI()
+	def __init__(self,vid_path):
+		self.makeFrames(vid_path)
+
+		self.bound_top_left = [-1,-1]
+		self.bound_bottom_right = [-1,-1]
+		self.channel_selections = [[0,0],[0,0]]
+		self.select_index = 0
+
+		self.root = tkinter.Tk()
+
+
+		self.pic_index = 0
+
+		raw_image = Image.fromarray(self.buf[self.pic_index])
+		self.pic_width, self.pic_height = raw_image.size
+
+
+		#Resize so our window isn't too big (for high res videos)
+		max_size = [1200,600] #Arbitrary
+		self.resize_ratio = 1
+		if(self.pic_width > max_size[0]):
+			self.resize_ratio = self.pic_width / max_size[0]
+
+		if(self.pic_height > max_size[1]):
+			self.resize_ratio = max(self.resize_ratio,self.pic_height/max_size[1])
+
+
+		if(self.resize_ratio > 1):
+			raw_image = raw_image.resize((round(self.pic_width / self.resize_ratio), round(self.pic_height / self.resize_ratio)))
+
+		self.img = ImageTk.PhotoImage(raw_image)
+
+		canvas_frame = tkinter.Frame(self.root) 
+		canvas_frame.pack()
+
+
+		fps_frame = tkinter.Frame(self.root)
+		fps_frame.pack(side="top")
+		fps_label = tkinter.Text(fps_frame,width=30,height=1)
+		fps_label.pack(side="left")
+		fps_label.insert("end","Frames per Second:")
+		fps_label.configure(state="disabled")
+		self.fps_entry = tkinter.Entry(fps_frame)
+		self.fps_entry.pack(side="left")
+
+		channel_frame = tkinter.Frame(self.root)
+		channel_frame.pack(side="top")
+		channel_label = tkinter.Text(channel_frame,width=30,height=1)
+		channel_label.pack(side="left")
+		channel_label.insert("end","Channel Width in micrometers:")
+		channel_label.configure(state="disabled")
+		self.channel_entry = tkinter.Entry(channel_frame)
+		self.channel_entry.pack(side="left")
+
+		image_selection_frame = tkinter.Frame(self.root)
+		image_selection_frame.pack(side="top")
+		next_button = ttk.Button(image_selection_frame, text='Next Frame',command = self.nextImage)
+		next_button.pack(side="left")
+		previous_button = ttk.Button(image_selection_frame, text='Previous Frame',command = self.previousImage)
+		previous_button.pack(side="left")
+
+
+		command_frame = tkinter.Frame(self.root)
+		command_frame.pack(side="top")
+		confirm_button = ttk.Button(command_frame, text='Confirm',command = self.confirm)
+		confirm_button.pack(side="left")
+		cancel_button = ttk.Button(command_frame, text='Cancel',command = self.cancel)
+		cancel_button.pack(side="left")
+
+
+
+
+		self.canvas = tkinter.Canvas(canvas_frame,width = self.pic_width / self.resize_ratio, height = self.pic_height / self.resize_ratio)
+		self.canvas.pack(side="top")
+		self.canvas.bind('<Button-1>', self.click)
+		self.drawGUI()
+
+		self.root.mainloop()
+
+
+
+	def makeFrames(self,vid_path):
+		if(vid_path!=""):
+			if(os.path.exists("frames/")):
+				shutil.rmtree('frames/')
+			os.mkdir('frames/')
+			os.system("ffmpeg -i " + vid_path + " -vf mpdecimate,setpts=N/FRAME_RATE/TB frames/%03d.png")
+
+		frame_list = sorted(os.listdir("frames"))
+		self.buf = numpy.empty((len(frame_list),),dtype = numpy.object_)
+		self.buf.fill([])
+		for i in tqdm(range(len(frame_list))):
+			self.buf[i] = cv2.imread("frames/"+frame_list[i])
+
+
+
+
+
+	def click(self,event):
+		modx = event.x * self.resize_ratio
+		mody = event.y * self.resize_ratio
+		if self.select_index == 0:
+			self.bound_top_left = [modx,mody]
+			self.select_index += 1
+		elif self.select_index == 1:
+			self.bound_bottom_right = [modx,mody]
+			self.drawGUI()
+			self.select_index += 1
+		elif self.select_index == 2:
+			self.channel_selections[0][0] = modx
+			self.channel_selections[0][1] = mody
+			self.select_index += 1
+		elif self.select_index == 3:
+			self.channel_selections[1][0] = modx
+			self.channel_selections[1][1] = mody
+			self.select_index += 1
+		self.drawGUI()
 
 
 		
 
-start_x = 0
-start_y = 0
-width = 0
-height = 0
-fps = 0
-conversion_factor = 1
-def confirm():
-	global start_x
-	global start_y
-	global width
-	global height
-	global fps
-	global conversion_factor
-	global wanted_start
+	def confirm(self):
+		if(self.select_index != 4):
+			return
 
-	if(select_index != 4):
-		return
+		start_x = round(self.bound_top_left[0])
+		start_y = round(self.bound_top_left[1])
+		width = round(abs(self.bound_bottom_right[0] - self.bound_top_left[0]))
+		height = round(abs(self.bound_bottom_right[1] - self.bound_top_left[1]))
+		fps = float(self.fps_entry.get())
+		channel_size_mm = float(self.channel_entry.get())
+		channel_size_px = float(abs(self.channel_selections[1][1] - self.channel_selections[0][1]))
+		conversion_factor = channel_size_mm/channel_size_px
 
-	start_x = round(bound_top_left[0])
-	start_y = round(bound_top_left[1])
-	width = round(bound_bottom_right[0] - bound_top_left[0])
-	height = round(bound_bottom_right[1] - bound_top_left[1])
-	fps = float(fps_entry.get())
-	channel_size_mm = float(channel_entry.get())
-	channel_size_px = float(abs(channel_selections[1][1] - channel_selections[0][1]))
-	conversion_factor = channel_size_mm/channel_size_px
+		self.root.destroy()
 
-	root.destroy()
+		subprocess.call("python3 uDROP_Generation_Standalone.py " + " ".join([str(fps),str(conversion_factor),str(start_x),str(start_y),str(width),str(height)]),shell=True)
+		#os.system("python3 generator_mine.py " + " ".join([str(fps),str(conversion_factor),str(start_x),str(start_y),str(width),str(height)]))
 
-	subprocess.call("python generator_mine.py " + " ".join([str(fps),str(conversion_factor),str(start_x),str(start_y),str(width),str(height)]),shell=True)
-	#os.system("python3 generator_mine.py " + " ".join([str(fps),str(conversion_factor),str(start_x),str(start_y),str(width),str(height)]))
-
-def drawGUI():
-	global select_index
-	global bound_top_left
-	global bound_bottom_right
-	canvas.create_image(0,0, anchor="nw", image=img)
+	def drawGUI(self):
+		self.canvas.create_image(0,0, anchor="nw", image=self.img)
 
 
-	mtext = "Press cancel to start over"
-	if select_index < 2:
-		mtext = "Draw bounding box"
-	elif select_index < 3:
-		mtext = "Draw channel top boundary"
-	elif select_index < 4:
-		mtext = "Draw channel bottom boundary"
+		mtext = "Press cancel to start over"
+		if self.select_index < 2:
+			mtext = "Draw bounding box"
+		elif self.select_index < 3:
+			mtext = "Draw channel top boundary"
+		elif self.select_index < 4:
+			mtext = "Draw channel bottom boundary"
 
-	
-	
-	canvas.create_text(10,10,fill="white",anchor="nw",font="Times 12",text=mtext)	
-	
+		
+		
+		self.canvas.create_text(10,10,fill="white",anchor="nw",font="Times 12",text=mtext)	
+		
 
 
 
-	if(select_index>1):
-		canvas.create_line(bound_top_left[0] / resize_ratio, bound_top_left[1] / resize_ratio, bound_top_left[0] / resize_ratio, bound_bottom_right[1] / resize_ratio, fill="green")
-		canvas.create_line(bound_top_left[0] / resize_ratio, bound_top_left[1] / resize_ratio, bound_bottom_right[0] / resize_ratio, bound_top_left[1] / resize_ratio, fill="green")
-		canvas.create_line(bound_top_left[0] / resize_ratio, bound_bottom_right[1] / resize_ratio, bound_bottom_right[0] / resize_ratio, bound_bottom_right[1] / resize_ratio, fill="green")
-		canvas.create_line(bound_bottom_right[0] / resize_ratio, bound_top_left[1] / resize_ratio, bound_bottom_right[0] / resize_ratio, bound_bottom_right[1] / resize_ratio, fill="green")
-	if(select_index>2):
-		for i in range(0,select_index-2):
-			canvas.create_line((channel_selections[i][0]/resize_ratio)-10, (channel_selections[i][1]/resize_ratio), (channel_selections[i][0]/resize_ratio)+10, (channel_selections[i][1]/resize_ratio), fill="red")
+		if(self.select_index>1):
+			self.canvas.create_line(self.bound_top_left[0] / self.resize_ratio, self.bound_top_left[1] / self.resize_ratio, self.bound_top_left[0] / self.resize_ratio, self.bound_bottom_right[1] / self.resize_ratio, fill="green")
+			self.canvas.create_line(self.bound_top_left[0] / self.resize_ratio, self.bound_top_left[1] / self.resize_ratio, self.bound_bottom_right[0] / self.resize_ratio, self.bound_top_left[1] / self.resize_ratio, fill="green")
+			self.canvas.create_line(self.bound_top_left[0] / self.resize_ratio, self.bound_bottom_right[1] / self.resize_ratio, self.bound_bottom_right[0] / self.resize_ratio, self.bound_bottom_right[1] / self.resize_ratio, fill="green")
+			self.canvas.create_line(self.bound_bottom_right[0] / self.resize_ratio, self.bound_top_left[1] / self.resize_ratio, self.bound_bottom_right[0] / self.resize_ratio, self.bound_bottom_right[1] / self.resize_ratio, fill="green")
+		if(self.select_index>2):
+			for i in range(0,self.select_index-2):
+				self.canvas.create_line((self.channel_selections[i][0]/self.resize_ratio)-10, (self.channel_selections[i][1]/self.resize_ratio), (self.channel_selections[i][0]/self.resize_ratio)+10, (self.channel_selections[i][1]/self.resize_ratio), fill="red")
 
 
-def cancel():
-	global bound_top_left
-	global bound_bottom_right
-	global select_index
-	select_index = 0
-	bound_top_left = [-1,-1]
-	bound_bottom_right = [-1,-1]
-	drawGUI()
+	def cancel(self):
+		self.select_index = 0
+		self.bound_top_left = [-1,-1]
+		self.bound_bottom_right = [-1,-1]
+		self.drawGUI()
 
 
-def nextImage():
-        global pic_index
-        global img
-        global buf
-        global resize_ratio
-        pic_index+=1
-        pic_index%=len(buf)
-        raw_image = Image.fromarray(buf[pic_index])
-        if(resize_ratio > 1):
-                raw_image = raw_image.resize((round(pic_width / resize_ratio), round(pic_height / resize_ratio)))
-        img = ImageTk.PhotoImage(raw_image)
-        drawGUI()
+	def nextImage(self):
+		self.pic_index+=1
+		self.pic_index%=len(self.buf)
+		raw_image = Image.fromarray(self.buf[self.pic_index])
+		if(self.resize_ratio > 1):
+			raw_image = raw_image.resize((round(self.pic_width / self.resize_ratio), round(self.pic_height / self.resize_ratio)))
+		self.img = ImageTk.PhotoImage(raw_image)
+		self.drawGUI()
 
 
-def previousImage():
-        global pic_index
-        global img
-        global buf
-        global resize_ratio
-        pic_index-=1
-        if(pic_index < 0):
-                pic_index=len(buf)-1
-        raw_image = Image.fromarray(buf[pic_index])
-        if(resize_ratio > 1):
-                raw_image = raw_image.resize((round(pic_width / resize_ratio), round(pic_height / resize_ratio)))
-        img = ImageTk.PhotoImage(raw_image)
-        drawGUI()
+	def previousImage(self):
+		self.pic_index-=1
+		if(self.pic_index < 0):
+			self.pic_index=len(self.buf)-1
+		raw_image = Image.fromarray(self.buf[self.pic_index])
+		if(self.resize_ratio > 1):
+			raw_image = raw_image.resize((round(self.pic_width / self.resize_ratio), round(self.pic_height / self.resize_ratio)))
+		self.img = ImageTk.PhotoImage(raw_image)
+		self.drawGUI()
+
+
+SetupGUI("")
 
 
 
 
-#TODO: add in selection of channel bottom and channel top. Selecting should be done on indexes now. Text box to show where we are at.
+
+
+
+
+
  
-root = tkinter.Tk()
-
-
-pic_index = 0
-
-pic_width = 0
-pic_height = 0
-raw_image = Image.fromarray(buf[pic_index])
-pic_width, pic_height = raw_image.size
-
-
-#Resize so our window isn't too big (for high res videos)
-max_size = [1200,600] #Arbitrary
-resize_ratio = 1
-if(pic_width > max_size[0]):
-	resize_ratio = pic_width / max_size[0]
-
-if(pic_height > max_size[1]):
-	resize_ratio = max(resize_ratio,pic_height/max_size[1])
-
-
-if(resize_ratio > 1):
-	raw_image = raw_image.resize((round(pic_width / resize_ratio), round(pic_height / resize_ratio)))
-
-img = ImageTk.PhotoImage(raw_image)
-
-canvas_frame = tkinter.Frame(root) 
-canvas_frame.pack()
-
-
-fps_frame = tkinter.Frame(root)
-fps_frame.pack(side="top")
-fps_label = tkinter.Text(fps_frame,width=30,height=1)
-fps_label.pack(side="left")
-fps_label.insert("end","Frames per Second:")
-fps_label.configure(state="disabled")
-fps_entry = tkinter.Entry(fps_frame)
-fps_entry.pack(side="left")
-
-channel_frame = tkinter.Frame(root)
-channel_frame.pack(side="top")
-channel_label = tkinter.Text(channel_frame,width=30,height=1)
-channel_label.pack(side="left")
-channel_label.insert("end","Channel Width in micrometers:")
-channel_label.configure(state="disabled")
-channel_entry = tkinter.Entry(channel_frame)
-channel_entry.pack(side="left")
-
-image_selection_frame = tkinter.Frame(root)
-image_selection_frame.pack(side="top")
-next_button = ttk.Button(image_selection_frame, text='Next Frame',command = nextImage)
-next_button.pack(side="left")
-previous_button = ttk.Button(image_selection_frame, text='Previous Frame',command = previousImage)
-previous_button.pack(side="left")
-
-
-command_frame = tkinter.Frame(root)
-command_frame.pack(side="top")
-confirm_button = ttk.Button(command_frame, text='Confirm',command = confirm)
-confirm_button.pack(side="left")
-cancel_button = ttk.Button(command_frame, text='Cancel',command = cancel)
-cancel_button.pack(side="left")
 
 
 
 
-canvas = tkinter.Canvas(canvas_frame,width = pic_width / resize_ratio,height=pic_height / resize_ratio)
-canvas.pack(side="top")
-canvas.bind('<Button-1>', click)
-drawGUI()
 
-root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
